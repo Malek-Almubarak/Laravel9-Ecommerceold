1976a282c9ad8ac3c3fda645f00be181f5461fba
Malek-Almubarak <2015710206067@ogrenci.karabuk.edu.tr>
2024-03-04 14:33:45 +0300

    welcome page changed

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,16 +1,13 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
     <x-application-logo class="block h-12 w-auto" />
-
+   
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
         Welcome to your Jetstream application!
     </h1>
 
-    <p class="mt-6 text-gray-500 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
-    </p>
+    <h1>welcome to my project</h1>
+    <h1>welcome to my project</h1>
+    <h1>welcome to my project</h1>
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
@@ -23,11 +20,11 @@
                 <a href="https://laravel.com/docs">Documentation</a>
             </h2>
         </div>
-
+        
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
             Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
         </p>
-
+       
         <p class="mt-4 text-sm">
             <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
                 Explore the documentation
